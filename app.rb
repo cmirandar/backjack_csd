c17c1feaf362c9b0d01aecb776696d07c90f4580
Cesar Miranda <cesarmiranda0992@gmail.com>
2016-12-16 20:41:43 +0000

    carta comodin

--- app.rb
+++ app.rb
@@ -19,24 +19,37 @@ get '/comenzar' do
     session["acumuladoPC"]  = 0
     session["cartaGeneradaJugador"] = ''
     session["cartaGeneradaPC"] = ''
+    session["cartaComodin"] = '' 
+    session["usasteComodin"] = 0 
     erb :solicitudCartas
 end
 
 get '/solicitarCarta' do
     valor = params["valor"]
-    random = Random.rand(13)
-    
-    
     if valor == '1' 
-       session["acumuladoJugador"] = random + session["acumuladoJugador"]
-       session["cartaGeneradaJugador"] = random.to_s + "+" + session["cartaGeneradaJugador"]
+        random = Random.rand(13)
+       if random + session["acumuladoJugador"] > 21  && session["usasteComodin"] == 0
+            session["cartaComodin"] = "Usaste tu carta comodin...Carta igual a" + random.to_s 
+            session["usasteComodin"] = 1 
+       else
+           session["acumuladoJugador"] = random + session["acumuladoJugador"]
+           session["cartaGeneradaJugador"] = random.to_s + "+" + session["cartaGeneradaJugador"]
+        end
    else
-        numeroJugadas = 3
+        numeroJugadas = 2
         i = 0
         while i < numeroJugadas  do
+            random = Random.rand(13)
             session["acumuladoPC"] = random + session["acumuladoPC"]
             session["cartaGeneradaPC"] = random.to_s + "+" + session["cartaGeneradaPC"]
            i +=1
+           if session["acumuladoPC"] > 21 
+                break;
+            else
+                if i == session["acumuladoPC"] - 1 and session["acumuladoPC"] < 21 and session["acumuladoPC"] < session["acumuladoJugador"]
+                    numeroJugadas +=1
+                end
+            end
         end
         
    end
